192af443091b9d2e72f844315fd81a402dec8c3d
luciandolo <75luciandolo@mail.com>
2023-08-11 11:57:57 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio>
+/**
+ * main -A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
