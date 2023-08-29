126be4d2c68c39beecf5037d741313560715fbed
luciandolo <75luciandolo@mail.com>
2023-08-29 06:18:17 -0700

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: area to fill with bytes
- * @b: constant byte
- * @n: number of bytes
- * Return: pointer to s
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
  */
 
-char *_memset(char *s, char b, unsigned int n)
+void print_diagsums(int *a, int size)
 {
-	unsigned int i;
+	int i, n, sum1 = 0, sum2 = 0;
 
-	for (i = 0 ; i < n ; i++)
-		s[i] = b;
-	return (s);
+	for (i = 0 ; i <= (size * size); i = i + size + 1)
+		sum1 = sum1 + a[i];
+
+	for (n = size - 1 ; n <= (size * size) - size; n = n + size - 1)
+		sum2 = sum2 + a[n];
+	printf("%d, %d\n", sum1, sum2);
 }
